2c9617af9a36c6cd960929d5908d9b2d67577a94
Filipe Valentim <filipevalentim0706@gmail.com>
2023-12-23 07:48:12 -0300

    SellersController criado, junto com seu index.cshtml em Views

---
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMVC.Controllers
+{
+  public class SellersController : Controller
+  {
+
+    public IActionResult Index()
+    {
+      ViewData["Message"] = DateTime.Now;
+      return View();
+    }
+  }
+}
