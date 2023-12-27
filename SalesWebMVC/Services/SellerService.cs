9b5635929182aa815a2379ab2354d8122a6e7f8e
Filipe Valentim <filipevalentim0706@gmail.com>
2023-12-27 19:03:13 -0300

    Seller details and eager loading

--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -1,4 +1,5 @@
-﻿using SalesWebMVC.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMVC.Data;
 using SalesWebMVC.Models;
 
 namespace SalesWebMVC.Services
@@ -23,7 +24,7 @@ namespace SalesWebMVC.Services
     }
     public Seller FindById(int id)
     {
-      return _context.Seller.FirstOrDefault(x => x.Id == id);
+      return _context.Seller.Include(obj => obj.Department).FirstOrDefault(x => x.Id == id);
     }
     public void Remove(int id)
     {
