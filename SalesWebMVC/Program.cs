a11071dc6c5aadd19ef4d9e0f7ce5da8e717ec3d
Filipe Valentim <filipevalentim0706@gmail.com>
2023-12-30 12:00:46 -0300

    Feito a classe Startup.cs

--- SalesWebMVC/Program.cs
+++ SalesWebMVC/Program.cs
@@ -9,43 +9,7 @@ namespace SalesWebMVC
   {
     public static void Main(string[] args)
     {
-      var builder = WebApplication.CreateBuilder(args);      
-      var connectionString = builder.Configuration.GetConnectionString("SalesWebMVCContext");
-      var serverVersion = ServerVersion.AutoDetect(connectionString);
-      builder.Services.AddDbContext<SalesWebMVCContext>
-        (options => options.UseMySql(connectionString, serverVersion, builder => builder.MigrationsAssembly("SalesWebMVC")));
-
-      builder.Services.AddScoped<SeedingService>();
-      builder.Services.AddScoped<SellerService>();
-      builder.Services.AddScoped<DepartmentService>();
-      // Add services to the container.
-      builder.Services.AddControllersWithViews();
-
-      var app = builder.Build();
-
-      // Configure the HTTP request pipeline.
-      if (!app.Environment.IsDevelopment())
-      {
-        app.UseExceptionHandler("/Home/Error");
-        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        app.UseHsts();
-
-      }
-      
-      app.Services.CreateScope().ServiceProvider.GetRequiredService<SeedingService>().Seed();
-
-      app.UseHttpsRedirection();
-      app.UseStaticFiles();
-
-      app.UseRouting();
-
-      app.UseAuthorization();
-
-      app.MapControllerRoute(
-          name: "default",
-          pattern: "{controller=Home}/{action=Index}/{id?}");
-
-      app.Run();
+      var builder = WebApplication.CreateBuilder(args).UseStartup<Startup>();
     }
   }
 }
---
+++ SalesWebMVC/Startup.cs
@@ -0,0 +1,76 @@
+﻿namespace SalesWebMVC;
+
+using Data;
+using Microsoft.EntityFrameworkCore;
+using Services;
+
+public interface IStartup
+  {
+    IConfiguration Configuration { get; }
+    public void ConfigureServices(IServiceCollection service);
+    public void Configure(WebApplication app, IWebHostEnvironment environment);
+  }
+public class Startup : IStartup
+  {
+    public IConfiguration Configuration { get; }
+
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection service)
+    {
+      var connectionString = Configuration.GetConnectionString("SalesWebMVCContext");
+      var serverVersion = ServerVersion.AutoDetect(connectionString);
+      service.AddDbContext<SalesWebMVCContext>(options => options.UseMySql(connectionString,
+      serverVersion,
+      builder => builder.MigrationsAssembly("SalesWebMVC")));
+      service.AddScoped<SeedingService>();
+      service.AddScoped<SellerService>();
+      service.AddScoped<DepartmentService>();
+      service.AddControllersWithViews();
+    }
+
+    public void Configure(WebApplication app, IWebHostEnvironment environment)
+    {
+      if (!app.Environment.IsDevelopment())
+      {
+        app.UseExceptionHandler("/Home/Error");
+        app.UseHsts();
+      }
+
+      app.Services.CreateScope().ServiceProvider.GetRequiredService<SeedingService>().Seed();
+
+      app.UseHttpsRedirection();
+      app.UseStaticFiles();
+
+      app.UseRouting();
+
+      app.UseAuthorization();
+
+      app.MapControllerRoute(
+      name: "default",
+      pattern: "{controller=Home}/{action=Index}/{id?}");
+    }
+  }
+public static class StartupExtensions
+  {
+    public static WebApplicationBuilder UseStartup<TStartup>(this WebApplicationBuilder webAppBuilder)
+        where TStartup : IStartup
+    {
+      var startup = Activator.CreateInstance(typeof(TStartup), webAppBuilder.Configuration) as IStartup;
+      if (startup == null)
+      {
+        throw new ArgumentException("Classe Startup.cs inválida!");
+      }
+
+      startup.ConfigureServices(webAppBuilder.Services);
+      var app = webAppBuilder.Build();
+
+      startup.Configure(app, app.Environment);
+      app.Run();
+
+      return webAppBuilder;
+    }
+  }
